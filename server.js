d7ca5c24a723485465c046ba9a5bbc0874ed50f6
Sebastian Schlegel <sebastian@coffeenerds.io>
2021-05-27 14:23:21 +0200

    Added error page

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(express.urlencoded({ extended: true }))
+
 //Set view engine
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
@@ -11,7 +13,17 @@ app.get("/", (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    
+    const { username, password } = req.body
+
+    if (!username || !password) {
+        res.render("error", {
+            message: "Bitte Benutername und Passwort angeben!",
+        })
+        return
+    }
+
+    console.log(req.body, username, password)
+    res.end()
 })
 
 app.listen(3000, '0.0.0.0', () => console.log("Server ready"));
